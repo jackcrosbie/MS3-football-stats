96b6ea5a00c7e6265d4b5db02c8e1897c5f59ed1
jackcrosbie <jack.crosbie@mycit.ie>
2021-09-15 14:48:58 +0000

    create function to gives user input options as strings

--- run.py
+++ run.py
@@ -1,3 +1,50 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('football_info')
+
+#manu = SHEET.worksheet('man_utd')
+#manc = SHEET.worksheet('man_city')
+#chelsea = SHEET.worksheet('chelsea')
+#livepool = SHEET.worksheet('liverpool')
+
+def user_commands():
+    """
+    gives commands the user is able to input to receive different data sets
+    """
+    print("Hi! Welcome to a football stats generator")
+    print("The available options are as follows")
+    
+    manu = SHEET.worksheet('man_utd')
+    manc = SHEET.worksheet('man_city')
+    chelsea = SHEET.worksheet('chelsea')
+    livepool = SHEET.worksheet('liverpool')
+
+    options = ['man united, man city, liverpool, chelsea']
+        
+    for i in range(len(options)):
+        print(str(i+1) + ":", options[i])
+
+
+    value = input("Please enter a string:\n")
+    print(f"You have entered {value}")
+        
+        
+
+user_commands()
+
+#def input_commands ()
+   # """
+   # Sets up the different string options that can be used as commands.
+    #Tells the user whether what they inputted in a valid command and if not,
+    #returns a message telling them it is not valid
+    #"""
