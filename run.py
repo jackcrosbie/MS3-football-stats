b4b9100b291e2faba058e7167e67ccb3c45476b6
jackcrosbie <jack.crosbie@mycit.ie>
2021-09-16 02:16:59 +0000

    add second function to run more user options

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,9 +14,9 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('football_info')
 
 data = {
-    "man_utd": SHEET.worksheet("man_utd").get_all_values(),
-    "man_city": SHEET.worksheet("man_city").get_all_values(),
-    "chelsea" : SHEET.worksheet('chelsea').get_all_values(),
+    "man united": SHEET.worksheet("man_utd").get_all_values(),
+    "man city": SHEET.worksheet("man_city").get_all_values(),
+    "chelsea": SHEET.worksheet('chelsea').get_all_values(),
     "liverpool": SHEET.worksheet('liverpool').get_all_values()
 }
 
@@ -29,20 +30,44 @@ def user_commands():
 
     options_2 = 'goalkeepers, defenders, midfielders, forwards'
     print(f"2: {options_2}")
-    
+
     options_3 = 'appearances, goals scored, clean sheets, age'
     print(f"3: {options_3}")
 
     value = input("Please enter a string:\n")
-    print(f"You have entered {value}")
+    print(f"You have entered {value}\n")
 
-    print(data[value])
-        
+    if value in ['man united', 'man city', 'liverpool', 'chelsea']:
+        print(tabulate(data[value]))
+    elif value not in ['man united', 'man city', 'liverpool', 'chelsea']:
+        print("You have not entered a correct option!")
+    else:
+        exit()
+
+
+    options = 'Back, Top Scorers, Appearances'
+    print(f"Next Options:")
+
 print("Hi! Welcome to a football stats generator")
 print("The available options are as follows:")
 
+def user_commands_2():
+
+    options_1 = 'goalkeepers, defenders, midfielders, forwards'
+    print(f"1: {options_1}")
+
+    options_2 = 'appearances, goals scored, clean sheets, age'
+    print(f"2: {options_2}")
+
+    value = input("Please enter a string:\n")
+    print(f"You have entered {value}\n")
+
 
 def main():
-    user_commands()  
+    """
+    Run all program functions
+    """
+    user_commands()
+    user_commands_2()
 
 main()
