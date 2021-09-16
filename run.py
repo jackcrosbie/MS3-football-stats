3585d09cb2ee91051bf719ff6bc359f08af3ba40
jackcrosbie <jack.crosbie@mycit.ie>
2021-09-16 20:30:52 +0000

    add function to show players with over 100 appearances for their team

--- run.py
+++ run.py
@@ -20,6 +20,9 @@ data = {
     "liverpool": SHEET.worksheet('liverpool').get_all_values()
 }
 
+team_name = ""
+position = ""
+
 options = ['return', 'age', 'goals scored', 'apperances', 'clean sheets', 'goalkeepers', 'defenders', 'midfielders', 'forwards', 'home']
 
 def user_commands():
@@ -39,53 +42,96 @@ def user_commands():
         team_name = input()
 
     print(tabulate(data[team_name]))
-    
+    return team_name 
 
-def user_commands_2():
+def user_commands_2(team_name):
 
-    options_1 = 'goalkeepers, defenders, midfielders, forwards, home'
+    options_1 = 'goalkeeper, defender, midfielder, forward, home'
     print(f"1: {options_1}")
 
     position = input("Please enter a string:\n")
     print(f"You have entered {position}\n")
 
-    while position not in (options):
+    while position not in (options_1):
         print("You entered a wrong option, Please enter a correct option")
         print(f"1: {options_1}")
         position = input()
 
     if position == 'home':
+        print("Hi! Welcome to a football stats generator")
+        print("The available options are as follows:")
         main()
+        
 
-    res = [i for i in data['man united'] if "Forward" in i]
-    
+    res = [i for i in data[team_name] if position.capitalize() in i]
     print(tabulate(res))
 
 print("Hi! Welcome to a football stats generator")
 print("The available options are as follows:")
 
-def top_scorer():
+def top_scorers():
     """
     Takes the goal scoring data from the spreadsheet and present it's to the user
-    with the list ordered by the most goals scored per team down to the least goals scored
+    with the list ordered by the most goals scored per team to the least.
     """
-    options = 'man united, man city, liverpool, chelsea'   
+    goal_scorers = []
+
+    options = 'man united, man city, liverpool, chelsea, home'   
     print(f"1: {options}")
     team_name = input("Please enter team name to see top scorers for that team:\n")
     print(f"you have entered: {team_name}")
 
-    res = [i for i in data['chelsea'] if "Goals Scored" in i]
-    res.col()
-    print(tabulate(res))
+    if team_name == 'home':
+        print("Hi! Welcome to a football stats generator")
+        print("The available options are as follows:")
+        main()
 
-    
+    for row in data[team_name]:
+        if "Goals Scored" in row:
+            continue
+        if int(row[3]) > 0:
+            goal_scorers.append(row)
+
+
+    goal_scorers.sort(key=lambda x: int(x[3]))
+    goal_scorers.reverse()
+    goal_scorers[:4]
+    print(tabulate(goal_scorers))
+
+def appearances():
+
+    player_apps = []
+
+    options = 'man united, man city, liverpool, chelsea, home'   
+    print(f"1: {options}")
+    team_name = input("Please enter team name to see players with over 100 appearances for that team:\n")
+    print(f"you have entered: {team_name}")
+
+    if team_name == 'home':
+        print("Hi! Welcome to a football stats generator")
+        print("The available options are as follows:")
+        main()
+
+    for row in data[team_name]:
+        if "Appearances" in row:
+            continue
+        if int(row[2]) > 100:
+            player_apps.append(row)
+
+
+    player_apps.sort(key=lambda x: int(x[2]))
+    player_apps.reverse()
+    player_apps[:4]
+    print(tabulate(player_apps))
+
 
 def main():
     """
     Run all program functions
     """
-    user_commands()
-    user_commands_2()
-    top_scorer()
+    team_name = user_commands()
+    user_commands_2(team_name)
+    top_scorers()
+    appearances()
 
 main()
